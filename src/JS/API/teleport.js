085654818f20b87445560576b8fe0324eda637fb
Andrea Ferrari <andreaferraridev@gmail.com>
2023-12-08 15:51:18 +0100

    asd

--- src/JS/API/teleport.js
+++ src/JS/API/teleport.js
@@ -1,17 +1,60 @@
 import axios from "axios";
-
-function teleportAPI() {
-  axios
-    .get("https://api.teleport.org/api/")
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => console.log(error));
-}
-
-export default teleportAPI;
+import _, { head } from "lodash";
+import Bloodhound from "typeahead.js/dist/bloodhound.min";
+import autocomplete from "typeahead.js/dist/typeahead.jquery.min";
 // Searching for Cities by Name
 
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+function handleSearch() {
+  const city = searchInput.value;
+  openCityPage(city);
+}
+
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    handleSearch();
+  }
+});
+
+searchButton.addEventListener("click", handleSearch);
+
+const city = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: {
+    url: "https://api.teleport.org/api/cities/?search=%QUERY",
+    wildcard: "%QUERY",
+  },
+});
+
+$("#searchInput").typeahead(null, {
+  name: "city",
+  display: "name",
+  source: city,
+  limit: 10,
+});
+
+/*function openCityPage(city) {
+  const teleportCityUrl = `https://www.teleport.org/cities/?search=${encodeURIComponent(
+    city
+  )}`;
+
+  axios
+    .get(teleportCityUrl)
+    .then((response) => {
+      const data = response.data;
+
+      window.location.href = "city-detail.html";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+*/
+
+export default city;
 // Basic information about a city
 
 // Life quality Data for Urban Area
--- src/JS/index.js
+++ src/JS/index.js
@@ -3,8 +3,8 @@ import _, { head } from "lodash";
 import "../assets/styles/styles.scss";
 import logo from "../assets/img/UrbanLeaf-Logo.png";
 import icon from "../assets/img/planet-earth.ico";
-import teleportAPI from "./API/teleport";
-import getAirAndWeatherData from "./API/iqair";
+import city from "./API/teleport.js";
+import getAirAndWeatherData from "./API/iqair.js";
 
 const logoImg = document.getElementById("logo");
 logoImg.src = logo;
@@ -12,24 +12,5 @@ logoImg.src = logo;
 const iconImg = document.getElementById("icon");
 iconImg.href = icon;
 
-document.getElementById("search-button").addEventListener("click", () => {
-  const searchInput = document.getElementById("search-input").value;
-
-  axios
-    .get(`https://api.unsplash.com/search/photos?query=${searchInput}`)
-    .then((response) => {
-      const resultContainer = document.getElementById("search-results");
-      resultContainer.innerHTML = "";
-
-      _.forEach(response.data, (result) => {
-        const resultDiv = document.createElement("div");
-        resultDiv.textContent = result.title;
-        resultContainer.appendChild(resultDiv);
-      });
-    })
-    .catch((error) => console.log(error));
-});
-
-console.log(teleportAPI());
 const cityToSearch = "Rome";
 getAirAndWeatherData(cityToSearch);
