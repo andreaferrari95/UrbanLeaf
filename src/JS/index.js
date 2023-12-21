81fcd54ba6e8b8a900599e1dcd2a06905a12ee31
Andrea Ferrari <andreaferraridev@gmail.com>
2023-12-21 15:24:16 +0100

    asd

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,8 +1,46 @@
 import axios from "axios";
-import _, { head } from "lodash";
+import _, { defer, head } from "lodash";
 import "../assets/styles/main.scss";
 import logo from "../assets/img/UrbanLeaf-Logo.png";
 import icon from "../assets/img/planet-earth.ico";
+
+//date and time function
+defer(() => {
+  let hrs = document.getElementById("hrs");
+  let min = document.getElementById("min");
+  let sec = document.getElementById("sec");
+  let day = document.getElementById("day");
+  let date = document.getElementById("date");
+  let month = document.getElementById("month");
+  let year = document.getElementById("year");
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  setInterval(() => {
+    let currentTime = new Date();
+
+    hrs.innerHTML =
+      (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML =
+      (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML =
+      (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+
+    day.innerHTML = daysOfWeek[currentTime.getDay()];
+
+    date.innerHTML = currentTime.getDate();
+    month.innerHTML = currentTime.getMonth();
+    year.innerHTML = currentTime.getFullYear();
+  }, 1000);
+});
 
 console.log(process.env.GOOGLE_PLACES_API);
 console.log(process.env.TELEPORT_API_URL);
